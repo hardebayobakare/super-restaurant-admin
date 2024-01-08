cb16287c00623f417ef449e13eec8ff76890a5f1
Adebayo Bakare <hardebayobakare@gmail.com>
2024-01-08 14:00:04 -0500

    Api List component creation

--- app/(dashboard)/[restaurantId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[restaurantId]/(routes)/billboards/components/client.tsx
@@ -1,12 +1,14 @@
 "use client"
+import { Plus } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
 
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
-import { Plus } from "lucide-react"
-import { useParams, useRouter } from "next/navigation"
-import { BillboardColumn, columns } from "./columns"
 import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
+
+import { BillboardColumn, columns } from "./columns"
 
 interface BillboardClientProps {
     data: BillboardColumn[]
@@ -32,6 +34,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
             </div>
             <Separator />
             <DataTable columns={columns} data={data} searchKey="label"/>
+            <Heading title="API" description="API calls for Billboard"/>
+            <Separator />
+            <ApiList entityName="billboards" entityIdName="billboardId"/>
         </>
     )
 }
