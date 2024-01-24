194e04495d2f78f39b86fde94d115524d7a0224d
Adebayo Bakare <hardebayobakare@gmail.com>
2024-01-24 17:24:39 -0500

    Quantity entity api route update

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -62,7 +62,9 @@ export async function POST(req: Request) {
                 }
             },
             data: {
-                isArchived: true
+                quantity: {
+                    increment: -1
+                }                    
             }
         })
     }
